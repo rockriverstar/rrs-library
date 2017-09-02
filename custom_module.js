1e59d6582cd7e3635ec868550937a8315d12db1d
Nathan Gasser <ngasser@rockriverstar.com>
2017-09-02 13:29:30 -0400

    Create custom_module.js

---
+++ custom_module.js
@@ -0,0 +1,10 @@
+/*global Drupal, jQuery */
+/*eslint no-shadow-restricted-names: "undefined"*/
+
+(function ($, Drupal, window, document, undefined) {
+  Drupal.behaviors.MODULENAME = {
+    attach: function(context, settings) {
+
+    }
+  };
+}) (jQuery, Drupal, this, this.document);
