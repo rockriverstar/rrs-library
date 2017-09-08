6fa37d8ffb850e02488318cffe1f74c42bde1afa
Nathan Gasser <ngasser@rockriverstar.com>
2017-09-08 06:56:52 -0400

    Create template.php
    Some useful template.php functions

---
+++ template.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Set the "placeholder" value of a text field in a form
+ * 'case' line holds ID of the <form>
+ * $form[''] holds the NAME from the <input> element
+ */
+ 
+function THEME_form_alter(&$form, &$form_state, $form_id) {
+  //dpm($form,$form['#id']);
+  switch ($form['#id']) {
+    case 'views-exposed-form-site-search-search-pane':
+      $form['search_api_views_fulltext']['#attributes']['placeholder'] = t('Search');
+      break;
+  }
+}
