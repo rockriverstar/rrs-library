ef783f0d044b422e52bd90357b072841204d6a9c
Nathan Gasser <ngasser@rockriverstar.com>
2018-01-16 12:42:33 -0500

    Update formapi-placeholder.php

--- formapi-placeholder.php
+++ formapi-placeholder.php
@@ -2,6 +2,7 @@
  * Add HTML5 Placeholder Text to search box
  */
 function MYTHEME_form_alter(&$form, &$form_state, $form_id){
+  dpm($form_id);
   if($form_id == "views_exposed_form"){
     if (isset($form['search_api_views_fulltext'])) {
             $form['search_api_views_fulltext']['#attributes'] = array('placeholder' => array(t('Search')));
