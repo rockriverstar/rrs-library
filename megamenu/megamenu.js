b4d3ee19954daef15b113bb1de9c8353fce3b98d
Nathan Gasser <ngasser@rockriverstar.com>
2018-06-04 08:58:48 -0400

    Update megamenu.js

--- megamenu/megamenu.js
+++ megamenu/megamenu.js
@@ -1,5 +1,7 @@
 
-
+// TBMegaMenu debug helper
 $(".tb-megamenu-item.dropdown .mega-dropdown-menu").dblclick(function() {
     	  $(this).closest('.tb-megamenu-item').toggleClass('debug');
 });
+$('body').append("<style id=\"tbmegadebug\">.tb-megamenu .mega.debug > .mega-dropdown-menu { display: block !important;}</style>");
+
